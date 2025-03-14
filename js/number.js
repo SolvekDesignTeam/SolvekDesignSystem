d2f0eaad0a5bbd67e2276b249a616a3a789c047e
ujunghee <zmfozj4172@naver.com>
2025-03-14 16:03:28 +0900

    number 추가

--- js/number.js
+++ js/number.js
@@ -1,9 +1,17 @@
 function initNumber() {
   
- const button = document.querySelector('.btn-left')
+  const input = document.querySelector(".input-number")
+  const plus = document.querySelector('.plus')
+  const minus = document.querySelector('.minus')
  
- if(button) {
-  const buttons = button.querySelectorAll('button')
-  console.log(buttons)
- }
+  if(input && plus && minus) {
+    plus.addEventListener("click", () => {
+      input.value = parseInt(input.value) + 1
+    })
+    minus.addEventListener("click", () => {
+      if(input.value > 0 ) {
+        input.value = parseInt(input.value) - 1
+      }
+    })
+  }
 }
